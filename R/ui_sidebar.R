e9afa13236085d91312691f78fcd6bbcc4fa6aef
cttnguyen <cnguyen6292@gmail.com>
2023-01-05 22:48:31 -0500

    organize tabs into budget parent

--- R/ui_sidebar.R
+++ R/ui_sidebar.R
@@ -1,12 +1,15 @@
 dbSide <- function(){ 
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Credit Cards", tabName = "credit_cards", icon = icon("credit-card")),
-      menuItem("Insurance", tabName = "insurance", icon = icon("umbrella")),
-      menuItem("Mortgage", tabName = "mortgage", icon = icon("house")),
-      menuItem("Savings", tabName = "savings", icon = icon("piggy-bank")),
-      menuItem("Subscriptions", tabName = "subscriptions", icon = icon("rotate")),
-      menuItem("Utilities", tabName = "utilities", icon = icon("plug")),
+      menuItem(
+        "Budget", icon = icon("money-bill-transfer"),
+        menuItem("Credit Cards", tabName = "credit_cards", icon = icon("credit-card")),
+        menuItem("Insurance", tabName = "insurance", icon = icon("umbrella")),
+        menuItem("Mortgage", tabName = "mortgage", icon = icon("house")),
+        menuItem("Savings", tabName = "savings", icon = icon("piggy-bank")),
+        menuItem("Subscriptions", tabName = "subscriptions", icon = icon("rotate")),
+        menuItem("Utilities", tabName = "utilities", icon = icon("plug"))
+      ),
       menuItem(
         "Retirement", icon = icon("person-cane"),
         menuSubItem("Projections", tabName = "projections", icon = icon("arrow-trend-up")),
